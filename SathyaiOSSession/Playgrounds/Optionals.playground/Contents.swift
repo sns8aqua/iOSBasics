98e4b8b9016b83d57108cb7e9319032d6d6b1b9e
SathyaNarayan <roflmeatu@gmail.com>
2022-01-08 00:48:29 +0100

    How to use + and how to unwrap boolean values?

--- SathyaiOSSession/Playgrounds/Optionals.playground/Contents.swift
+++ SathyaiOSSession/Playgrounds/Optionals.playground/Contents.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-
+// ------------------------------------------------------------------
 // Variables should always start with small case
 // Best practice is to name the variable with the data type
 var name: String?
@@ -15,5 +15,34 @@ print(name ?? "NoName")
 if let age = age {
     print(age)
 }
-    
+// ------------------------------------------------------------------
+// ADD TWO NUMBERS
 
+func addTwoNumbers(number1: Int?,number2: Int?) -> Int? {
+    var sumOfTwoNumbers: Int?
+//    sumOfTwoNumbers = number1? + number2?
+    return sumOfTwoNumbers
+}
+
+// ------------------------------------------------------------------
+
+// ARRAYS
+// Find number 7 in string of arrays
+func countArrrayOfIntegers(numbers: Int?) -> Bool? {
+    // Declare an Array
+    var numberOfArray: [Int]?
+    var checkNumber: Bool?
+    // Allocate memory for the Array
+    checkNumber = false
+    numberOfArray = [Int]()
+    numberOfArray = [1,2,3,4,7]
+    if let numberOfArray = numberOfArray {
+        if numberOfArray.contains(7) {
+            checkNumber = true
+            return checkNumber
+        }
+    }
+    return checkNumber
+}
+
+print(countArrrayOfIntegers(numbers: 7))
